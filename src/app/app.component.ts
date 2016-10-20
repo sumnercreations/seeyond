3371e9e8d59871ee5a1a4dfb053b7ea92e56fef0
Ammon Lockwood <ammon@plaidtie.net>
2016-10-20 16:38:00 -0600

    Feat: Seeyond Design Tool - the app has begun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'seeyond works!';
+  header = 'Welcome to the Seeyond Design Tool';
+  title = 'Begin by choosing the type of feature you are working on';
+  features = [
+	{
+		type: 'wall',
+		title: 'Wall Feature',
+		image: 'http://dummyimage.com/300x300'
+	},
+	{
+		type: 'wall_to_ceiling',
+		title: 'Wall-to-Ceiling Feature',
+		image: 'http://dummyimage.com/300x300'
+	},
+	{
+		type: 'linear_partition',
+		title: 'Freestanding Linear Partition',
+		image: 'http://dummyimage.com/300x300'
+	},
+	{
+		type: 'curved_partition',
+		title: 'Freestanding Curved Partition',
+		image: 'http://dummyimage.com/300x300'
+	}
+  ]
 }
