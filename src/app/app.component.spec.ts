350e4493bc8085d3b9cb96a52703344bc4e047cc
Ammon Lockwood <ammon@plaidtie.net>
2016-10-20 16:45:21 -0600

    Fix: Fixes lint and unit test errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,26 @@ describe('App: Seeyond', () => {
   it(`should have as title 'app works!'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('seeyond works!');
+    expect(app.title).toEqual('Begin by choosing the type of feature you are working on');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a p tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('seeyond works!');
+    expect(compiled.querySelector('p').textContent).toContain('Begin by choosing the type of feature you are working on');
+  }));
+
+  it(`should have as header 'Welcome to the Seeyond Design Tool'`, async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.header).toEqual('Welcome to the Seeyond Design Tool');
+  }));
+
+  it('should render header in a h1 tag', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the Seeyond Design Tool');
   }));
 });
