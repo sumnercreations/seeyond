38724278f156bae4b44a0f0136705c3c618fbba5
Ammon Lockwood <ammon@plaidtie.net>
2016-12-08 17:04:10 -0700

    Checkpoint: Generating the necessary components take 2

---
+++ src/app/feature.ts
@@ -0,0 +1,22 @@
+export class Feature {
+	selectedFeature: string;
+	type: string;
+	title: string;
+	image: string;
+
+	constructor() {
+		let selectedFeature = '';
+		let type = 'feature';
+		let title = 'Feature';
+		let image = 'http://images.3-form.com/seeyond/feature.jpg';
+		let params = [];
+	}
+}
+
+export class Params {
+	length: number;
+	width: number;
+	height: number;
+	patternStrength: number;
+	angle: number;
+}
---
+++ src/app/seeyond/seeyond.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'seeyond-seeyond',
+  templateUrl: './seeyond.component.html',
+  styleUrls: ['./seeyond.component.css']
+})
+export class SeeyondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
