413dacb2ca9e292b18ba7eab4b97310a7d0862aa
Ammon Lockwood <ammon@plaidtie.net>
2017-01-17 10:49:49 -0700

    Feat: implements the tessellation and visualization modules
    - The code is commented out due to the issues on bitbucket #1 (on both repos visualization and tessellation)

--- src/app/seeyond/feature.ts
+++ src/app/seeyond/feature.ts
@@ -23,5 +23,24 @@ export class Feature {
     this.title = feature.title;
     this.measurements = feature.measurements;
     this.image = feature.image;
+
+    // update the visualization
+    var syd_t = require( 'syd-tessellation' );
+    var syd_v = require( 'syd-visualization' );
+    // syd_t.QT.SetUserDataProperties(
+    //   {
+    //     Type:0,
+    //     Tessellation:0,
+    //     Width:120,
+    //     Height:60,
+    //     Radius:60,
+    //     Angle:0
+    //   }
+    // );
+    // syd_t.QT.SetUserDataProperties({});
+    syd_t.QT.SetTessellationArray({});
+    syd_t.QT.UpdateFeature();
+    var data = syd_t.QT.GetTessellationArray();
+    // syd_v.QT.Visualization.visualizeWall(data, 8, 6, 0xff9933);
   }
 }
--- src/app/seeyond/visualization/visualization.component.ts
+++ src/app/seeyond/visualization/visualization.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Feature } from '../Feature';
 
 @Component({
   selector: 'seeyond-visualization',
@@ -7,14 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private feature: Feature) { }
 
 ngOnInit() {
-    var syd_t = require( 'syd-tessellation' );
-    var syd_v = require( 'syd-visualization' );
-    
-    syd_t.QT.UpdateFeature();
-    var data = syd_t.QT.GetTessellationArray();
-    syd_v.QT.Visualization.visualizeWall(data, 8, 6, 0x80ff00);
   }
 }
