e529fa3ba3335edc0a05023e7c6820d02ea10d94
Ammon Lockwood <ammon@plaidtie.net>
2017-02-02 20:19:20 -0700

    Checkpoint: working on the design tab
    - we are now able to select pattern
    - able to change the patternstrength
    - able to select a material

--- src/app/seeyond/design/design.component.ts
+++ src/app/seeyond/design/design.component.ts
@@ -6,11 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./design.component.css']
 })
 export class DesignComponent implements OnInit {
+  // tooltip position
   position = 'above';
+
+  // slider settings
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 10;
+  min = 1;
+  showTicks = true;
+  tickInterval = 1;
+  step = 1;
+  thumbLabel = true;
+  patternStrength = 1;
+  vertical = false;
+
+  selectedPattern = 'billow';
+  selectedMaterial = 'burnt_umber';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public updateSelectedPattern(pattern: string) {
+    this.selectedPattern = pattern;
+  }
+
+  public updateSelectedMaterial(material: string) {
+    this.selectedMaterial = material;
+  }
+
+  public updatePatternStrength(strength: number) {
+    console.log(strength);
+    this.patternStrength = strength;
   }
 
 }
