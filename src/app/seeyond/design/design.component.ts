e0b957400f1a5478acad4864f35562295ef67474
Ammon Lockwood <ammon@plaidtie.net>
2017-02-06 14:03:29 -0700

    Feat: connections have been made
    - the inputs from the dimensions and design are now connected
    with the tessellation and visualization libraries.
    - we have some things to iron out and a few tessellations
    that are not working
    - also, this tool sending the type to the visualization isn't working
    correctly.

--- src/app/seeyond/design/design.component.ts
+++ src/app/seeyond/design/design.component.ts
@@ -10,7 +10,7 @@ export class DesignComponent implements OnInit {
   // tooltip position
   position = 'above';
   // Selected Defaults
-  selectedPattern = this.feature.pattern? this.feature.pattern : 'billow';
+  selectedTessellation = this.feature.tessellation? this.feature.tessellation : 'billow';
   selectedMaterial = this.feature.material? this.feature.material : 'burnt_umber';
   patternStrength = this.feature.patternStrength? this.feature.patternStrength : 3;
   strengths = [1,2,3,4,5,6,7,8,9,10];
@@ -21,9 +21,43 @@ export class DesignComponent implements OnInit {
     console.log(this.feature);
   }
 
-  public updateSelectedPattern(pattern: string) {
-    console.log(pattern);
-    this.selectedPattern = this.feature.pattern = pattern;
+  public updateSelectedTessellation(tessellation: string) {
+    console.log(tessellation);
+    var tessellations: {
+      0: "court",
+      1: "cusp",
+      2: "kink",
+      3: "tilt",
+      4: "billow"
+    };
+
+    switch (tessellation) {
+      case "court":
+        this.feature.tessellation = 0;
+        break;
+
+      case "cusp":
+        this.feature.tessellation = 1;
+        break;
+
+      case "kink":
+        this.feature.tessellation = 2;
+        break;
+
+      case "tilt":
+        this.feature.tessellation = 3;
+        break;
+
+      case "billow":
+        this.feature.tessellation = 4;
+        break;
+
+      default:
+        alert(tessellation + " is not a supported tessellation");
+        break;
+    }
+    // this.selectedTessellation = this.feature.tessellation = tessellation;
+    this.selectedTessellation = tessellation;
 
     // update the visualization
     this.feature.reloadVisualization()
