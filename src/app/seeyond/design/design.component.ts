9655058089f2c1b49c1d2db45c0d672bef6bcb23
Ammon Lockwood <ammon@plaidtie.net>
2017-02-06 11:16:28 -0700

    Feat: design and dimensions updating feature
    - The feature singleton is updating when selections are made
    - I need to still get the update to send to the visualization correctly.
    Once that is done the visualization should show the changes we make.

--- src/app/seeyond/design/design.component.ts
+++ src/app/seeyond/design/design.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Feature } from '../Feature';
 
 @Component({
   selector: 'seeyond-design',
@@ -9,26 +10,39 @@ export class DesignComponent implements OnInit {
   // tooltip position
   position = 'above';
   // Selected Defaults
-  selectedPattern = 'billow';
-  selectedMaterial = 'burnt_umber';
-  patternStrength = 3;
+  selectedPattern = this.feature.pattern? this.feature.pattern : 'billow';
+  selectedMaterial = this.feature.material? this.feature.material : 'burnt_umber';
+  patternStrength = this.feature.patternStrength? this.feature.patternStrength : 3;
+  strengths = [1,2,3,4,5,6,7,8,9,10];
 
-  constructor() { }
+  constructor(private feature: Feature) { }
 
   ngOnInit() {
+    console.log(this.feature);
   }
 
   public updateSelectedPattern(pattern: string) {
-    this.selectedPattern = pattern;
+    console.log(pattern);
+    this.selectedPattern = this.feature.pattern = pattern;
+
+    // update the visualization
+    this.feature.reloadVisualization()
   }
 
   public updateSelectedMaterial(material: string) {
-    this.selectedMaterial = material;
+    console.log(material);
+    this.selectedMaterial = this.feature.material = material;
+
+    // update the visualization
+    this.feature.reloadVisualization()
   }
 
   public updatePatternStrength(strength: number) {
     console.log(strength);
-    this.patternStrength = strength;
+    this.patternStrength = this.feature.patternStrength = strength;
+
+    // update the visualization
+    this.feature.reloadVisualization()
   }
 
 }
